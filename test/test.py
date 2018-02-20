d3729569675da8626fbcce9d0b92f516789060b3
Aric Huang <arichuang@gmail.com>
2018-02-19 21:47:53 -0800

    add network enable/disable commands, test cases

--- test/test.py
+++ test/test.py
@@ -24,17 +24,51 @@ HTTPS_IMAGE_URL = 'https://i.imgur.com/3YBkyf6.jpg'
 def fetch(url):
     return requests.get(url, proxies=proxies, verify=False)
 
+
 def clear_cache():
     result = s.rm('-rf /data/funes_rmb_cache/*').run()
     if result.rc != 0:
         raise Exception('Failed to clear cache: %s' % result.stderr)
 
 
+def disable_network():
+    result = s.ifconfig('eth0 down').run()
+    if result.rc != 0:
+        raise Exception('Failed to disable network: %s' % result.stderr)
+
+
+def enable_network():
+    result = s.ifconfig('eth0 up').run()
+    if result.rc != 0:
+        raise Exception('Failed to enable network: %s' % result.stderr)
+
+    result = s.route('add default gw 172.20.0.1 eth0').run()
+    if result.rc != 0:
+        raise Exception('Failed to set default gateway: %d' % result.rc)
+
+
 clear_cache()
-time.sleep(2)  # wait for nginx to start
+time.sleep(3)  # wait for nginx to start
 
 
 class CacheTests(unittest.TestCase):
+
+    def test_get_http_image(self):
+        r = fetch(HTTP_IMAGE_URL)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Funes-Cache-Status'], 'MISS')
+
+        r = fetch(HTTP_IMAGE_URL)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Funes-Cache-Status'], 'HIT')
+
+        disable_network()
+
+        r = fetch(HTTP_IMAGE_URL)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Funes-Cache-Status'], 'HIT')
+
+        enable_network()
 
     def test_get_https_image(self):
         r = fetch(HTTPS_IMAGE_URL)
@@ -45,11 +79,10 @@ class CacheTests(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.headers['Funes-Cache-Status'], 'HIT')
 
-    def test_get_http_image(self):
-        r = fetch(HTTP_IMAGE_URL)
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.headers['Funes-Cache-Status'], 'MISS')
+        disable_network()
 
-        r = fetch(HTTP_IMAGE_URL)
+        r = fetch(HTTPS_IMAGE_URL)
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.headers['Funes-Cache-Status'], 'HIT')
+
+        enable_network()
