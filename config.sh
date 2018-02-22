7f218ace0b00f9f7494ff987b2d5247da751a0a3
Aric Huang <arichuang@gmail.com>
2018-02-22 13:07:54 -0800

    allow disabling transparent proxy

--- config.sh
+++ config.sh
@@ -6,6 +6,14 @@ mkdir -p ./build/certs
 cp ./conf/nginx.conf ./build/conf
 cp ./conf/nginx.conf.server ./build/conf
 
+# Enable transparent mode if not explicitly disabled
+mkdir -p ./build/conf/transparent_proxy
+if [ -z "${DISABLE_TRANSPARENT_PROXY}" ]; then
+    cp ./conf/transparent_proxy/*.conf ./build/conf/transparent_proxy
+else
+    rm -rf ./build/conf/transparent_proxy/*
+fi
+
 cp ./certs/nginx.crt ./build/conf
 cp ./certs/nginx.key ./build/conf
 cp ./certs/cacert.pem ./build/conf
