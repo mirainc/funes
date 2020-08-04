7e5329247cca42f4cf2654c00632b91fff4127bd
Aric Huang <arichuang@gmail.com>
2020-08-03 17:54:31 -0700

    OPTIONS request caching

--- patch.sh
+++ patch.sh
@@ -5,5 +5,6 @@ START_DIR=$(pwd)
 cd ./extract/nginx-1.18.0
 
 patch -p1 < $START_DIR/ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_1018.patch
+patch -p1 < $START_DIR/allow_options_cache.patch
 
 touch $START_DIR/patch
