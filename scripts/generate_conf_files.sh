ef071cf1c60d49a66b23ae5c784193f7c20f6841
Aric Huang <arichuang@gmail.com>
2020-08-07 17:57:46 -0700

    test

--- scripts/generate_conf_files.sh
+++ scripts/generate_conf_files.sh
@@ -63,6 +63,7 @@ fi
 echo "Nameserver is: $NAMESERVER"
 
 echo "Copying nginx config"
+ls
 envsubst '${LOG_DIR}' < ./conf/nginx.conf.template > ./conf/nginx.conf
 envsubst '${NAMESERVER} ${LOG_DIR} ${CONTENT_CACHE_DIR} ${CONTENT_CACHE_KEYS_ZONE} ${CONTENT_CACHE_SIZE}' < ./conf/nginx.conf.server.template > ./conf/nginx.conf.server
 envsubst '${ROOT_CA_CERT} ${ROOT_CA_KEY}' < ./conf/generate_ssl_certs.template.lua > ./conf/generate_ssl_certs.lua
