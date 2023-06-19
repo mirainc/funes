73e316fdb60a0d3cf7a4362a7c36606cc09b1f9d
Jordan Schroter <github@jschr.io>
2023-06-19 13:18:08 -0400

    Fix tests (#2)
    * Debug
    
    * Debug 2
    
    * Debug 3
    
    * Debug 4
    
    * Debug 5
    
    * Move test assets to Raydiant s3 bucket

--- test/test.py
+++ test/test.py
@@ -19,11 +19,11 @@ proxies = {
 }
 
 
-HTTP_IMAGE_URL = 'http://www.mixedcontentexamples.com/Content/Test/steveholt.jpg'
-HTTPS_IMAGE_URL = 'https://i.imgur.com/3YBkyf6.jpg'
-HTTPS_VIDEO_URL = 'https://dl6.webmfiles.org/big-buck-bunny_trailer.webm'
-RSS_URL = 'http://www.avclub.com/rss'
-HLS_STREAM_URL = 'http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8'
+HTTP_IMAGE_URL = 'http://mira-test-assets.s3.us-west-2.amazonaws.com/funes/image.jpg'
+HTTPS_IMAGE_URL = 'https://mira-test-assets.s3.us-west-2.amazonaws.com/funes/image.jpg'
+HTTPS_VIDEO_URL = 'https://mira-test-assets.s3.us-west-2.amazonaws.com/funes/video.webm'
+RSS_URL = 'http://mira-test-assets.s3.us-west-2.amazonaws.com/funes/rss.xml'
+HLS_STREAM_URL = 'http://mira-test-assets.s3.us-west-2.amazonaws.com/funes/hls/hls.m3u8'
 EXPIRED_SSL_URL = 'https://expired.badssl.com'
 
 
