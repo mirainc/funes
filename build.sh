dc14f98c0a82ddc61d1b3b53215748793a2bb453
Zilvinas Rudzionis <3447630+zrudzionis@users.noreply.github.com>
2024-04-18 16:07:57 +0300

    Fix empty reply

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@ set -e
 
 START_DIR=$(pwd)
 
-cd ./extract/openresty-1.17.8.2
+cd ./extract/openresty-1.25.3.1
 
 ./configure --prefix=$START_DIR/build --add-module=$START_DIR/ngx_http_proxy_connect_module --with-http_ssl_module --with-http_slice_module --with-luajit
 
 # Patch nginx
-patch -d build/nginx-1.17.8/ -p 1 < $START_DIR/ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_1018.patch
-patch -d build/nginx-1.17.8/ -p 1 < $START_DIR/patches/allow_options_cache.patch
-patch -d build/nginx-1.17.8/ -p 1 < $START_DIR/patches/force_cacheable.patch
+patch -d build/nginx-1.25.3/ -p 1 < $START_DIR/ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_102101.patch
+patch -d build/nginx-1.25.3/ -p 1 < $START_DIR/patches/allow_options_cache.patch
+patch -d build/nginx-1.25.3/ -p 1 < $START_DIR/patches/force_cacheable.patch
 
 make && make install
